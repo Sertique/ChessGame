d67cb23e391ebae8b94059083a34ab6dcd1b5fbb
Sertique <Sertique@proton.me>
2023-12-28 10:42:23 +0100

    rien

--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
-#include "Echiquier.cpp"
+#include "raylib.h"
 
-using namespace std;
+// À ne pas lancer car fait lagguer l'ordinateur
 
 int main()
 {
-    /* pour l'instant : mettre la 1ere lettre de la couleur du pion joué en minuscule */
-    Echiquier echiquier;
-      return 0;
+  const int windowWidth(800);
+  const int windowHeigth(450);
+
+  InitWindow(windowWidth, windowHeigth, "Test Window");
+  SetTargetFPS(30);
+
+  Texture2D array[32];
+
+  Image wp = LoadImage("/test");
+  ImageResize(Image *image, int newWidth, int newHeight)
+  array[0] = LoadTextureFromImage(wp);
+
+  while (!WindowShouldClose())
+  {
+    BeginDrawing();
+
+    ClearBackground(LIGHTGRAY);
+    //DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+    EndDrawing();
+  }
+  CloseWindow();
+
+  return 0;
 }
