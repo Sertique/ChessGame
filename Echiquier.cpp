744cc67a1e3150648674569187f2721ff271e46b
Sertique <Sertique@proton.me>
2023-12-29 20:37:03 +0100

    Affichage des pièces fonctionnels - Next purpose : to create all pieces

--- Echiquier.cpp
+++ Echiquier.cpp
@@ -1,21 +1,23 @@
 #include "Echiquier.hpp"
 #include <raylib.h>
 #include "Piece.hpp"
+#include <iostream>
+#include <vector>
 
 int Echiquier::m_cellSize(100); /* */ int Echiquier::get_cellSize() { return m_cellSize; }
+
 Color Echiquier::get_Color(piece_color color) {
-  if (color == White)
-  {
-    return WHITE;
-  } else {
-    return BLACK;
-  }
+  if (color == White) { return WHITE;
+  } else { return BLACK; }
 }
+
+// Color Echiquier::get_inverseColor(piece_color color) {
+//   if (color == W hite) { return BLACK;
+//   } else { return WHITE; }
+// }
 
 Echiquier::Echiquier()
 {
-  pieceInitialization(king, White, 4, 0);
-
 // Cells color initialization
   for (int i(0) ; i < 8 ; i++)
   {
@@ -38,11 +40,16 @@ Echiquier::Echiquier()
       chessBoard[Piece::get_positionZ(j, i)].xOrigin = x;
       chessBoard[Piece::get_positionZ(j, i)].yOrigin = y;
       chessBoard[Piece::get_positionZ(j, i)].cellSize = m_cellSize;
+      chessBoard[Piece::get_positionZ(j, i)].pieceContent = nullptr;
       x += 100;
     }
     x = m_cellSize;
     y -= 100;
   }
+/* Piece initialization */
+  pieceInitialization(king, White, 4, 0);
+  pieceInitialization(king, Black, 4, 7);
+
 }
 
 
@@ -54,11 +61,23 @@ bool Echiquier::estPair(int x)
 
 void Echiquier::drawChessboard()
 {
+  vector<int> coordinatesPieceToDraw;
+  vector<int>::iterator it;
+
   for (int i(0) ; i < 64 ; i++)
   {
     DrawRectangle(chessBoard[i].xOrigin, chessBoard[i].yOrigin, chessBoard[i].cellSize, chessBoard[i].cellSize, Echiquier::get_Color(chessBoard[i].colorOfCell));
-    DrawTexture(chessBoard[4].pieceContent->get_pieceTexture(), 500, 300, WHITE);
+    if (chessBoard[i].pieceContent != nullptr)
+    {
+      coordinatesPieceToDraw.push_back(i);
+    }
   }
+
+  for (it = coordinatesPieceToDraw.begin() ; it != coordinatesPieceToDraw.end() ; it++)
+  {
+    DrawTexture(chessBoard[*it].pieceContent->get_pieceTexture(), chessBoard[*it].xOrigin, chessBoard[*it].yOrigin, WHITE);
+  }
+  
 }
 
 
@@ -73,6 +92,8 @@ void Echiquier::pieceInitialization(piece_type pieceType, piece_color colorPiece
     } else {
       path = "./chessPieceImage/bk.png";
     }
-    chessBoard[Piece::get_positionZ(posX, posY)].pieceContent = new King(colorPiece, posX, posY, path.c_str());
+    chessBoard[Piece::get_positionZ(posX, posY)].pieceContent = new King(colorPiece, posX, posY, path);
+  } else {
+    std::cout << "Pas de pièce initialisée" << std::endl;
   }
 }
